71b8afe2bd4de40329c3ac7bfd15e6b1cf0c139b
Rishabh Rai <108413835+rishabhrai11@users.noreply.github.com>
2024-10-11 03:07:27 +0530

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/HospitalDB";
-    private static final String USER = "root";  // Replace with your MySQL user name
+    private static final String USER = "";  // Replace with your MySQL user name
     private static final String PASSWORD = "";  // Replace with your MySQL password
 
     public static Connection getConnection() throws SQLException {
